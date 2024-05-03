171086a75b959ce21fee02a2721f9c5a5bf115d2
Tsukasa <tsukasa.heiwa@gmail.com>
2024-05-04 01:10:13 +0530

    Configure project

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App: React.FC = () => {
+  return <div>Tarots Here</div>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+
+root.render(<App />);
